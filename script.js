a726f71ec53b10654e05c00c2022c729bfb4ca6d
hmzq-404 <66130102+hmzq-404@users.noreply.github.com>
2023-07-24 17:47:05 +0500

    Learn basics of javascript

---
+++ script.js
@@ -0,0 +1,21 @@
+function add7(number) {
+    return number + 7;
+}
+
+
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+
+function capitalize(text) {
+    return text.toUpperCase()[0] + text.substr(1, text.length)
+}
+
+function lastLetter(text) {
+    return text[text.length-1]
+}
+
+
+
+
+console.log(lastLetter("United States"));
