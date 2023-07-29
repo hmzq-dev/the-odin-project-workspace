0fdbdc3a26ef41802de75cfb53bab2b41662e879
hmzq-404 <66130102+hmzq-404@users.noreply.github.com>
2023-07-29 15:16:17 +0500

    Learn DOM Manipulation and event listeners

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-function getPrimeNumbers(end) {
-    const primeNumbers = [];
-    primeLoop: 
-    for (let num = 2; num <= end; num++) {
-        for (let divisor = 2; divisor < num; divisor++) {
-            if (num % divisor === 0) {
-                continue primeLoop;
-            }
-        }
-        primeNumbers.push(num)
-    }
-    return primeNumbers;
-}
+const btn = document.querySelector("#btn");
 
-console.log(getPrimeNumbers(100).join(", "))
+btn.addEventListener("click", (e) => {
+    console.dir(e.target);
+});
