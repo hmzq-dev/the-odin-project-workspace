dd02dc40f56c8a5b95a750df23ce27cbf8afd8ac
hmzq-404 <66130102+hmzq-404@users.noreply.github.com>
2023-09-01 20:01:04 +0500

    Learn about positioning

---
+++ script.js
@@ -0,0 +1,10 @@
+/*  
+top, left, right, bottom: work on all elements that are not static
+
+Absolute: Remove element from normal document flow
+
+Fixed: Remove element from normal document flow and position it relative to viewport
+
+Sticky: Act as a normal element until you scroll completely past them, must have a top/bottom/left/right property
+
+*/
