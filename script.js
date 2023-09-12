f7581519693f3dca8752a951818be158a81a0070
hmzq-404 <66130102+hmzq-404@users.noreply.github.com>
2023-09-12 22:43:25 +0500

    Start MDN's tutorial on forms

--- script.js
+++
@@ -1,5 +0,0 @@
-document.querySelector("button").addEventListener("click", () => {
-    const root = document.documentElement;
-    const newTheme = root.className === "dark" ? "light" : "dark";
-    root.className = newTheme;
-})
