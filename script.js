5ed61845b77281157f82e568ec0bf5f383953bad
hmzq-404 <66130102+hmzq-404@users.noreply.github.com>
2023-12-30 19:46:50 +0500

    Start learning about factory functions

--- script.js
+++ script.js
@@ -1,34 +1,18 @@
-Hero.prototype.greet = function() {
-    return `${this.name} says hello`;
+function createUser (name) {
+    const discordName = "@" + name;
+  
+    let reputation = 0;
+    const getReputation = () => reputation;
+    const giveReputation = () => reputation++;
+  
+    return { name, discordName, getReputation, giveReputation };
 }
+  
+const josh = createUser("josh");
+josh.giveReputation();
+josh.giveReputation();
 
-function Hero(name, level) {
-    this.name = name;
-    this.level = level;
-}
-
-function Warrior(name, level, weapon) {
-    Hero.call(this, name, level);
-    this.weapon = weapon;
-}
-
-function Healer(name, level, spell) {
-    Hero.call(this, name, level);
-    this.spell = spell;
-}
-
-Object.setPrototypeOf(Warrior.prototype, Hero.prototype);
-Object.setPrototypeOf(Healer.prototype, Hero.prototype);
-
-
-Warrior.prototype.attack = function () {
-  return `${this.name} attacks with the ${this.weapon}.`;
-}
-
-Healer.prototype.heal = function () {
-  return `${this.name} casts ${this.spell}.`;
-}
-
-
-const hero1 = new Warrior('Bjorn', 1, 'axe');
-const hero2 = new Healer('Kanin', 1, 'cure');
+console.log({
+    discordName: josh.discordName,
+    reputation: josh.getReputation()
+});
